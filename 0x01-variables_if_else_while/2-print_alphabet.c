d1d83fd0e335b65e9cc539c124f10a0690706ce5
Noxolo <Noxoloprettykhoza@gmail.com>
2023-07-14 09:49:58 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Print alphabet lowercase'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar(65);
+	putchar('\n');
+	return (0);
+}

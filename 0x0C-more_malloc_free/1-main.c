c1eb51ec12b49efa2375470529a4178020aaa3bc
Noxolo <Noxoloprettykhoza@gmail.com>
2023-08-10 06:49:39 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-main.c
+++ 0x0C-more_malloc_free/1-main.c
@@ -3,15 +3,15 @@
 #include <stdlib.h>
 
 /**
- * main - check the code
+ * main - check the code for ALX School students.
  *
  * Return: Always 0.
  */
 int main(void)
-
+{
 	char *concat;
 
-	concat = string_nconcat("Best ", "School !!!", 6);
+	concat = string_nconcat("Holberton ", "School !!!", 6);
 	printf("%s\n", concat);
 	free(concat);
 	return (0);

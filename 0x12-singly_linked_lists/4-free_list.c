c432455c10f0aa7b887be2cf50ff1de21a145f47
Noxolo01 <Noxoloprettykhoza@gmail.com>
2023-08-30 02:39:41 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - A function that frees a list
+ * @head: pointer to the list head
+ *
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+}

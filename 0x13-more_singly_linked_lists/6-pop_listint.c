9540ce4ecdbe981bcb8b860fd1a4b00ae0cda4f2
Noxolo01 <Noxoloprettykhoza@gmail.com>
2023-08-30 05:34:54 -0700

    6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - A function that deletes the head node
+ * of a listint_t linked list
+ * @head: pointer to the first node in the list
+ *
+ * Return: data(n)
+ */
+
+int pop_listint(listint_t **head)
+{
+	int copy;
+	listint_t *temp;
+
+	if (*head == NULL)
+		return (0);
+	temp = *head;
+	*head = (*head)->next;
+	copy = temp->n;
+	free(temp);
+	return (copy);
+}
---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - A function that returns the nth node of a list
+ * @head: pointer to first element in the list
+ * @index: index list
+ *
+ * Return: nth element
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *temp = head;
+	unsigned int count = 0;
+
+	if (head == NULL)
+		return (NULL);
+	while (count < index)
+	{
+		count++;
+		temp = temp->next;
+		if (temp == NULL)
+			return (NULL);
+	}
+	return (temp);
+}
